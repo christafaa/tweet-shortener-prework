a29aade0e15051415494a65b04e1415128b8b684
Chris Alberti <christopherjalberti@gmail.com>
2018-04-20 20:00:03 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,4 +1,10 @@
 def word_substituter
-  
+  dictionary = {"hello" becomes 'hi'
+"to, two, too" become '2' 
+"for, four" become '4'
+'be' becomes 'b'
+'you' becomes 'u'
+"at" becomes "@" 
+"and" becomes "&"}
 
 end
