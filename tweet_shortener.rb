fa7f773d061884c259405dffad2bd08c858be969
Chris Alberti <christopherjalberti@gmail.com>
2018-04-20 20:01:25 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,13 +1,15 @@
 def word_substituter
   dictionary = {
     "hello" => 'hi',
-    "to" => '2'
-    "two" => '2'
-    "too" => '2'
-    "for, four" => '4'
-    'be' => 'b'
-    'you' => 'u'
-    "at" => "@"
-    "and" => "&"}
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "four" => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@",
+    "and" => "&"
+  }
 
 end
